5bf042e60e42a04c50cdd90715bd2720d9717cae
kimtk <vodjaos@gmail.com>
2023-07-27 11:20:18 +0900

    Radio Button 완료

---
+++ src/components/MuiRadioButton.jsx
@@ -0,0 +1,44 @@
+import {
+  Box,
+  FormControl,
+  FormControlLabel,
+  FormHelperText,
+  FormLabel,
+  Radio,
+  RadioGroup,
+} from "@mui/material";
+import React, { useState } from "react";
+
+export const MuiRadioButton = () => {
+  const [value, setValue] = useState("");
+  console.log(value);
+  const handleChange = (e) => {
+    setValue(e.target.value);
+  };
+
+  return (
+    <Box>
+      <FormControl>
+        <FormLabel id="job-experience-group-label">
+          Years of experience
+        </FormLabel>
+        <RadioGroup
+          name="job-experience-group"
+          aria-labelledby="job-experience-group-label"
+          value={value}
+          onChange={handleChange}
+          row
+        >
+          <FormControlLabel
+            control={<Radio size="small" color="secondary" />}
+            label="0-2"
+            value="0-2"
+          />
+          <FormControlLabel control={<Radio />} label="3-5" value="3-5" />
+          <FormControlLabel control={<Radio />} label="6-10" value="6-10" />
+        </RadioGroup>
+        <FormHelperText>Invalid selection</FormHelperText>
+      </FormControl>
+    </Box>
+  );
+};
